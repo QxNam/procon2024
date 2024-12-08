bb3315a839f51a896d07ebe761a3f737142574a5
Trịnh Thị Bảo Bảo <baobao@Bao-Bao-2.local>
2024-12-08 17:19:30 +0700

    check

--- api/config.py
+++
@@ -1,9 +0,0 @@
-
-PROCON_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjQsIm5hbWUiOiJEdW1wbGluZ0NvZGUiLCJpc19hZG1pbiI6ZmFsc2UsImlhdCI6MTczMjYwNDgxNSwiZXhwIjoxNzMyNzc3NjE1fQ.gYr9GLbM4TdkbWxltAV-2LNX3uihkgl2B2gCxLLPpRE'
-
-URL = "https://proconvn.duckdns.org"
-HEADERS = {"Authorization": PROCON_TOKEN}
-
-if __name__ == '__main__':
-    print(HEADERS)
-    
